4fc3afec1648e2854c3312d3a8507d495619530a
Isabela <isabela_cmoreira@hotmail.com>
2022-03-19 19:43:07 -0300

    Button that starts the game was componentized and named HomeButton

---
+++ src/components/HomeButton/index.js
@@ -0,0 +1,13 @@
+import  {ButtonStyle,Typography} from './style';
+
+
+export default function HomeButton(){
+    return(
+    <ButtonStyle>
+        <Typography> Iniciar Recall!</Typography>
+    </ButtonStyle>
+    );
+}
+
+
+ 
---
+++ src/components/HomeButton/style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const ButtonStyle = styled.button `
+height: 31px;
+width: 141px;
+border-radius: 5px;
+border: 1px solid #D70900
+background-color: #FFFFFF;
+margin-top: 73px;
+`;
+
+const Typography = styled.p `
+font-family: Recursive;
+color: #D70900;
+font-size: 12px;
+font-style: normal;
+font-weight: 400;
+line-height: 14px;
+letter-spacing: 0em;
+text-align: center;
+
+`
+export {
+    ButtonStyle,
+    Typography
+}
