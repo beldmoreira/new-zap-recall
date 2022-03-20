3d8d878b531ce2d6e59967a7fca0ea6a696f350d
Isabela <isabela_cmoreira@hotmail.com>
2022-03-20 17:46:40 -0300

    Red button inside the flashcard was made into a component

---
+++ src/components/RedButton/index.js
@@ -0,0 +1,10 @@
+import { RedLayout, Typography } from "./style";
+
+export default function RedButton (){
+    return(
+    <RedLayout>  
+    <Typography>Não lembrei</Typography>  
+    </RedLayout>  
+    );
+}
+
