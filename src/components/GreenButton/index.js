de169c546a2daf82436c0205a0291387f9dea20a
Isabela <isabela_cmoreira@hotmail.com>
2022-03-20 17:45:59 -0300

    Green button inside the card was made into a component

---
+++ src/components/GreenButton/index.js
@@ -0,0 +1,11 @@
+import { GreenLayout, Typography } from "./style";
+
+
+export default function GreenButton(){
+    return (
+    <GreenLayout>
+        <Typography> Zap! </Typography>
+    </GreenLayout>    
+
+    );
+}
