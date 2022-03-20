8cf5b20c8d90f541bbf6ac4c81e9abab3f331025
Isabela <isabela_cmoreira@hotmail.com>
2022-03-20 17:48:58 -0300

    Yellow button inside the flashcard was made into a component

---
+++ src/components/YellowButton/index.js
@@ -0,0 +1,9 @@
+import { Typography, YellowLayout } from "./style";
+
+export default function YellowButton(){
+    return(
+<YellowLayout>
+    <Typography> Quase não lembrei </Typography>
+</YellowLayout>
+    );
+}
---
+++ src/components/YellowButton/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const Typography = styled.p `
+font-family: Recursive;
+font-size: 7px;
+font-style: normal;
+font-weight: 400;
+line-height: 8px;
+letter-spacing: 0em;
+text-align: center;
+color: #FFFFFF;
+`; 
+const YellowLayout = styled.div `
+height: 24px;
+width: 55px;
+border-radius: 5px;
+background-color:#FF922E;
+`;
+
+export{
+    Typography,
+    YellowLayout
+}
