aac4f92e57df663b433e729385292a4df06d618f
Isabela <isabela_cmoreira@hotmail.com>
2022-03-21 15:02:16 -0300

    Style adjustments

---
+++ src/components/DeckPage/style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const DeckPageTop = styled.div `
+background-color: #FB6B6B;
+min-height: 100vh;
+`;
+const Container = styled.div `
+display: block;
+align-self: flex-start;
+padding-top:42px;
+`;
+const Typography = styled.p`
+font-family: Righteous;
+font-size: 36px;
+font-style: normal;
+font-weight: 400;
+line-height: 45px;
+letter-spacing: -0.012em;
+text-align: center;
+color: #FFFFFF;
+`;
+export {
+    DeckPageTop,
+    Container,
+    Typography
+}
