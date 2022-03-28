82a38cfcbcd582726c1659267fcf6c51db8de4d1
Lígia D'Ávila Bozzi <ligia.bozzi@gmail.com>
2022-03-28 09:39:17 -0300

    preparing review

--- src/components/RestartButton/index.js
+++
@@ -1,9 +0,0 @@
-import { PinkButton, Typography } from "./style";
-
-export default function RestartButton(){
-    return(
-    <PinkButton>
-        <Typography> REINICIAR RECALL </Typography>
-    </PinkButton>
-    );
-}
